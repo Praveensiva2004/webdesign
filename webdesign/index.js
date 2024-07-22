f761ef09be2eafeb9d7ffa70a6720c1bc450e476
Praveen s <166095477+Praveensiva2004@users.noreply.github.com>
2024-07-22 16:48:46 +0530

    Add files via upload

---
+++ webdesign/index.js
@@ -0,0 +1,18 @@
+
+        function buttonA(){
+            document.getElementById("option_a").style="background-color:red;";
+            document.getElementById("option_a_ans").style="background-color:red;";
+         }
+
+        function buttonB(){
+            document.getElementById("option_b").style="background-color:red;";
+            document.getElementById("option_b_ans").style="background-color:red;";
+        }
+        function buttonC(){
+            document.getElementById("option_c").style="background-color:red;";
+            document.getElementById("option_c_ans").style="background-color:red;";
+        }
+        function buttonD(){
+            document.getElementById("option_d").style="background-color:green;";
+            document.getElementById("option_d_ans").style="background-color:green;";
+         }
